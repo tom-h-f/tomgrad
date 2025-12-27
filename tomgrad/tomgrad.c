693575ccb8732ae1e30938aa134b8035f314c62e
Tom French <thomas.harry.french@icloud.com>
2025-12-27 17:35:42 +0000

    feat: fully incorporate shape into the tensor struct

--- tomgrad/tomgrad.c
+++ tomgrad/tomgrad.c
@@ -6,18 +6,24 @@
 #include <string.h>
 
 
-tg_err_t tensor_init(tg_tensor_shape_t* shape, tg_tensor_t** ptr) {
-    assert(shape != NULL);
-    assert(shape->dimensions != NULL);
-    assert(shape->n_dimensions > 0);
+tg_err_t tensor_init(size_t dims[], size_t n_dims, tg_tensor_t** ptr) {
+    assert(dims != NULL);
+    assert(n_dims > 0);
 
-    tg_tensor_t* tensor = (tg_tensor_t*)calloc(1, sizeof(tg_tensor_t));
+
+    size_t total_size = \
+        (total_elements_for_dimensions(dims, n_dims) * \
+        sizeof(tg_value_t)) + \
+        sizeof(tg_tensor_t);
+
+    tg_tensor_t* tensor = calloc(1, total_size);
     if (!tensor) {return ERR_MEMORY_ALLOCATION; }
+    tensor_shape_init(dims, n_dims, &tensor->shape);
 
-    tensor->size = tensor_shape_total_elements(shape);
-    tensor->vals = (tg_value_t*)calloc(tensor->size, sizeof(tg_value_t));
+    tensor->size = tensor_total_elements(tensor);
+    tensor->vals = (tg_value_t*)(tensor+1);
 
-    assert(tensor->size = tensor_shape_total_elements(shape));
+    assert(tensor->size == tensor_total_elements(tensor));
     for(size_t i = 0; i< tensor->size; i++) {
         assert(tensor->vals[i] == 0);
     }
@@ -25,6 +31,7 @@ tg_err_t tensor_init(tg_tensor_shape_t* shape, tg_tensor_t** ptr) {
     *ptr = tensor;
     return SUCCESS;
 }
+
 
 tg_err_t tensor_scalar_add(tg_tensor_t* tensor, tg_value_t scalar) {
       TENSOR_SCALAR_OP(tensor, scalar, +);
@@ -45,65 +52,57 @@ tg_err_t tensor_scalar_div(tg_tensor_t* tensor, tg_value_t scalar) {
 
 tg_err_t tensor_sqrt(tg_tensor_t* tensor) {
     for (size_t i = 0; i < (tensor)->size; i++) { 
-          (tensor)->vals[i] = sqrtl((tensor)->vals[i]); 
+          (tensor)->vals[i] = sqrtf((tensor)->vals[i]); 
     }
     return SUCCESS;
 }
 
-tg_err_t  tensor_free(tg_tensor_t* tensor) {
-    assert(tensor != NULL);
-    free(tensor->vals);
-    free(tensor);
+tg_err_t tensor_abs(tg_tensor_t* tensor) {
+    for (size_t i = 0; i < (tensor)->size; i++) { 
+          (tensor)->vals[i] = fabsf((tensor)->vals[i]); 
+    }
     return SUCCESS;
 }
-
 
 void tensor_print(tg_tensor_t* tensor) {
     for(size_t i = 0; i< tensor->size; i++) {
-        printf("[%.6Lf] ", tensor->vals[i]);
-        if (i != 0 && (i+1) % 6 == 0) { printf("\n");}
+        printf("[%09.03f] ", tensor->vals[i]);
+        if (i != 0 && (i+1) % 3 == 0) { printf("\n");}
     }
 }
 
 
-tg_err_t tensor_shape_init(size_t dims[], size_t n_dims, tg_tensor_shape_t** ptr) {
+tg_err_t tensor_shape_init(size_t dims[], size_t n_dims, tg_tensor_shape_t* shape) {
     assert(dims != NULL);
     assert(n_dims > 0);
-
-    tg_tensor_shape_t* shape = (tg_tensor_shape_t*)calloc(1, sizeof(tg_tensor_shape_t));
     if (!shape) {return ERR_MEMORY_ALLOCATION; }
 
     shape->n_dimensions = n_dims;
-    shape->dimensions = (size_t*)calloc(n_dims, sizeof(size_t));
-    memcpy((void*)shape->dimensions, \
-           (const void*)dims, \
-           n_dims*sizeof(size_t));
+    shape->dimensions = dims;
 
+    assert(&shape->dimensions != &dims);
     assert(*dims == *shape->dimensions);
 
     // TODO: Calculate strides
-
-    *ptr = shape;
     return SUCCESS;
 }
 
-size_t tensor_shape_total_elements(tg_tensor_shape_t* shape) {
-    assert(shape != NULL);
-    assert(shape->n_dimensions > 0);
+size_t tensor_total_elements(tg_tensor_t* tensor) {
+    assert(tensor->shape.n_dimensions > 0);
 
     size_t n = 1;
-    for(size_t i = 0; i < shape->n_dimensions; i++) {
-        n *= shape->dimensions[i];
+    for(size_t i = 0; i < tensor->shape.n_dimensions; i++) {
+        n *= tensor->shape.dimensions[i];
     }
+
+    total_elements_for_dimensions(tensor->shape.dimensions, tensor->shape.n_dimensions);
     return n;
 }
 
-
-
-
-tg_err_t tensor_shape_free(tg_tensor_shape_t* shape) {
-    free(shape->dimensions);
-    free(shape->strides);
-    free(shape);
-    return SUCCESS;
+size_t total_elements_for_dimensions(size_t dims[], size_t n_dims) {
+    size_t n = 1;
+    for(size_t i = 0; i < n_dims; i++) {
+        n *= dims[i];
+    }
+    return n;
 }
