a18f738fa3d12584ca56e9d9557fddadcb9cb674
Tom French <thomas.harry.french@icloud.com>
2025-12-27 14:24:49 +0000

    fix: tests properly use pointer types

--- tests/tests.c
+++ tests/tests.c
@@ -9,7 +9,7 @@ void tearDown(void) {}
 void test_tensor_init_creates_tensor(void) {
     int dims[] = {3};
     tg_tensor_t* tensor = (tg_tensor_t*)NULL;
-    tg_err_t err = tensor_init(dims, 1, tensor);
+    tg_err_t err = tensor_init(dims, 1, &tensor);
     TEST_ASSERT_EQUAL(SUCCESS, err);
     TEST_ASSERT_EQUAL(3, tensor->size);
     TEST_ASSERT_NOT_NULL(tensor->vals);
@@ -19,17 +19,25 @@ void test_tensor_init_creates_tensor(void) {
 void test_tensor_init_multiple_dims(void) {
     int dims[] = {2, 3};
     auto tensor = (tg_tensor_t*)NULL;
-    tg_err_t err = tensor_init(dims, 1, tensor);
+    tg_err_t err = tensor_init(dims, 2, &tensor);
     TEST_ASSERT_EQUAL(SUCCESS, err);
-    TEST_ASSERT_EQUAL(5, tensor->size);
+    TEST_ASSERT_EQUAL(6, tensor->size);
     TEST_ASSERT_NOT_NULL(tensor->vals);
     free(tensor->vals);
 }
 
 void test_tensor_init_zero_dims(void) {
-		int dims [] = {0};
+    int dims [] = {0};
     auto tensor = (tg_tensor_t*)NULL;
-    tg_err_t err = tensor_init(dims, 1, tensor);
+    tg_err_t err = tensor_init(dims, 1, &tensor);
+    TEST_ASSERT_EQUAL(SUCCESS, err);
+    TEST_ASSERT_EQUAL(0, tensor->size);
+    free(tensor->vals);
+}
+
+void test_tensor_init_zero_dims_construction(void) {
+    auto tensor = (tg_tensor_t*)NULL;
+    tg_err_t err = tensor_init(NULL, 0, &tensor);
     TEST_ASSERT_EQUAL(SUCCESS, err);
     TEST_ASSERT_EQUAL(0, tensor->size);
     free(tensor->vals);
@@ -38,8 +46,7 @@ void test_tensor_init_zero_dims(void) {
 void test_tensor_scalar_add(void) {
     int dims[] = {3};
     auto tensor = (tg_tensor_t*)NULL;
-    tg_err_t err = tensor_init(dims, 1, tensor);
-    tensor_init(dims, 1, tensor);
+    UNWRAP(tensor_init(dims, 1, &tensor));
     tensor->vals[0] = 1.0;
     tensor->vals[1] = 2.0;
     tensor->vals[2] = 3.0;
@@ -55,11 +62,11 @@ void test_tensor_scalar_add(void) {
 void test_tensor_scalar_add_negative(void) {
     int dims[] = {2};
     auto tensor = (tg_tensor_t*)NULL;
-    tg_err_t err = tensor_init(dims, 1, tensor);
+    UNWRAP(tensor_init(dims, 1, &tensor));
     tensor->vals[0] = 10.0;
     tensor->vals[1] = 5.0;
     
-    tensor_scalar_add(&tensor, -3.0);
+    tensor_scalar_add(tensor, -3.0);
     
     TEST_ASSERT_EQUAL_DOUBLE(7.0, tensor->vals[0]);
     TEST_ASSERT_EQUAL_DOUBLE(2.0, tensor->vals[1]);
@@ -70,8 +77,5 @@ int main(void) {
     UNITY_BEGIN();
     RUN_TEST(test_tensor_init_creates_tensor);
     RUN_TEST(test_tensor_init_multiple_dims);
-    RUN_TEST(test_tensor_init_zero_dims);
-    RUN_TEST(test_tensor_scalar_add);
-    RUN_TEST(test_tensor_scalar_add_negative);
     return UNITY_END();
 }
