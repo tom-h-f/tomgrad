ed46aae303efb020f564b51f53d4a7f7c0e7d4fb
Tom French <thomas.harry.french@icloud.com>
2025-12-28 03:49:32 +0000

    feat: adjustments to scratchpad for manual testing etc

--- scratchpad/scratchpad.c
+++ scratchpad/scratchpad.c
@@ -5,9 +5,22 @@
 #include <math.h>
 
 int main(void) {
-		int dims[] = {6, 6, 6};
-		tg_tensor_t* ptr = (tg_tensor_t*)NULL;
-		UNWRAP(tensor_init(dims, 2, &ptr));
-		tensor_scalar_add(ptr, 2.0);
-		return 0;
+
+    tg_tensor_t* a = (tg_tensor_t*)NULL;
+    TENSOR_CREATE_FILLED(&a, 1.5, 2, 3, 10);
+    tg_tensor_t* b = (tg_tensor_t*)NULL;
+    TENSOR_CREATE_FILLED(&b, 2.25, 2, 3, 10);
+
+    tensor_print(a);
+    tensor_print(b);
+
+    tg_tensor_t* tensor = tensor_mul(a, b);
+
+    TENSOR_PRINT_GRADIENTS(tensor->input_tensors[0]);
+    tensor->backward(tensor);
+    TENSOR_PRINT_GRADIENTS(tensor->input_tensors[0]);
+
+    printf("\n%lu\n", tensor->n_elements);
+
+    return 0;
 }
